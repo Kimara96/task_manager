f1e29174293d918ca7b06c065e720e541ec718b0
Kimara96 <marthakissi96@gmail.com>
2025-08-11 11:00:42 +0000

    getting started with task manager

---
+++ main.py
@@ -0,0 +1,7 @@
+# Ask user to input 
+first_name= input ("enter first name;")
+# Ask user to enter last name
+last_name = input ("enter last name;")
+full_name = first_name + " " + last_name
+# Print user's fullname in uppercase
+print (full_name.upper())
